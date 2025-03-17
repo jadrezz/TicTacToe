3cd0c3c5ee63746d21161264676bfe179abdd8ac
jadrezz <jadrez.g@gmail.com>
2025-03-18 00:49:54 +0300

    Add colors and few fixs

--- main.go
+++ main.go
@@ -9,6 +9,13 @@ import (
 	"unicode"
 )
 
+const (
+	red    string = "\033[0;31m%s\033[0m"
+	yellow string = "\033[1;33m%s\033[0m"
+	green  string = "\033[0;32m%s\033[0m"
+	cyan   string = "\033[0;36m%s\033[0m"
+)
+
 var winCombinations = [][]int{
 	{0, 1, 2}, // Первая строка
 	{3, 4, 5}, // Вторая строка
@@ -20,15 +27,10 @@ var winCombinations = [][]int{
 	{2, 4, 6}, // Побочная диагональ
 }
 
-type cell struct {
-	id    int
-	value rune
-}
-
-func drawBoard(board []cell) {
+func drawBoard(board []rune) {
 	fmt.Println(strings.Repeat("_", 11))
 	for n, v := range board {
-		fmt.Printf("|%c| ", v.value)
+		fmt.Printf("|%c| ", v)
 		if (n+1)%3 == 0 {
 			fmt.Println()
 			continue
@@ -37,27 +39,27 @@ func drawBoard(board []cell) {
 	fmt.Println(strings.Repeat("_", 11))
 }
 
-func makeStep(board []cell, num int, playerFigure rune) bool {
-	if num < 0 || num > 9 || board[num].value == 'X' || board[num].value == 'Y' {
+func makeStep(board []rune, num int, playerFigure rune) bool {
+	if (num < 0 || num >= 9) || (board[num] == 'X' || board[num] == 'Y') {
 		return false
 	}
-	board[num].value = playerFigure
+	board[num] = playerFigure
 	return true
 }
 
-func isWin(board []cell) bool {
+func isWin(board []rune) bool {
 	for _, combo := range winCombinations {
 		posA, posB, posC := combo[0], combo[1], combo[2]
-		if board[posA].value == board[posB].value && board[posB].value == board[posC].value {
+		if board[posA] == board[posB] && board[posB] == board[posC] {
 			return true
 		}
 	}
 	return false
 }
 
-func isDraw(board []cell) bool {
-	for _, set := range board {
-		if unicode.IsNumber(set.value) {
+func isDraw(board []rune) bool {
+	for _, val := range board {
+		if unicode.IsDigit(val) {
 			return false
 		}
 	}
@@ -65,39 +67,35 @@ func isDraw(board []cell) bool {
 }
 
 func main() {
-	board := []cell{
-		{1, '1'}, {3, '2'}, {3, '3'},
-		{4, '4'}, {5, '5'}, {6, '6'},
-		{7, '7'}, {8, '8'}, {9, '9'},
-	}
+	board := []rune{'1', '2', '3', '4', '5', '6', '7', '8', '9'}
 	player := 'X'
 	input := bufio.NewScanner(os.Stdin)
 
 	for {
 		drawBoard(board)
-		fmt.Printf("Игрок %c, сделайте ход\n", player)
+		fmt.Println(fmt.Sprintf(cyan, fmt.Sprintf("Игрок %c, сделайте ход", player)))
 
 		input.Scan()
 		n, err := strconv.Atoi(input.Text())
 		if err != nil {
-			fmt.Println("Некорректный ввод")
+			fmt.Println(fmt.Sprintf(red, "Некорректный ввод"))
 			continue
 		}
 
 		if !makeStep(board, n-1, player) {
-			fmt.Println("Выход за пределы доски или занятая клетка")
+			fmt.Println(fmt.Sprintf(red, "Выход за пределы доски или занятая клетка"))
 			continue
 		}
 
 		if isWin(board) {
 			drawBoard(board)
-			fmt.Printf("Победили %c\n", player)
+			fmt.Println(fmt.Sprintf(green, fmt.Sprintf("Победили %c\n", player)))
 			break
 		}
 
 		if isDraw(board) {
 			drawBoard(board)
-			fmt.Println("Ничья!")
+			fmt.Println(fmt.Sprintf(yellow, "Ничья!"))
 			break
 		}
 
